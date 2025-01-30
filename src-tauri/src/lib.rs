1efd0e654e798f93c5c19a53f7fb9e0574e4f831
macaujack <yanxfu@gmail.com>
2025-01-30 23:58:10 +0800

    Add a tray icon

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,8 @@
 use tauri::{
     menu::{Menu, MenuItem},
-    tray::TrayIconBuilder,
-    PhysicalPosition, PhysicalSize, Position, Size, WebviewUrl,
+    tray::{MouseButton, MouseButtonState, TrayIconBuilder, TrayIconEvent},
+    AppHandle, Manager, PhysicalPosition, PhysicalSize, Position, Size, WebviewUrl,
+    WebviewWindowBuilder,
 };
 
 mod config;
@@ -19,14 +20,22 @@ pub fn run() {
             let (config, is_first_start) = config::read_config();
 
             let webview_url_reader = WebviewUrl::App("index.html".into());
-            let window_reader = tauri::WebviewWindowBuilder::new(app, "main", webview_url_reader)
-                .title("Sneaky Reader")
-                .decorations(false)
-                .shadow(false)
-                .transparent(true)
-                .always_on_top(true)
-                .build()
-                .unwrap();
+            let mut window_builder_reader =
+                tauri::WebviewWindowBuilder::new(app, "main", webview_url_reader)
+                    .title("Sneaky Reader")
+                    .decorations(false)
+                    .shadow(false)
+                    .transparent(true)
+                    .always_on_top(true);
+            #[cfg(not(target_os = "macos"))]
+            {
+                window_builder_reader = window_builder_reader.skip_taskbar(true);
+            }
+            #[cfg(target_os = "macos")]
+            {
+                app.set_activation_policy(tauri::ActivationPolicy::Accessory);
+            }
+            let window_reader = window_builder_reader.build().unwrap();
 
             window_reader
                 .set_ignore_cursor_events(true)
@@ -44,39 +53,70 @@ pub fn run() {
                 }))
                 .expect("Cannot set window position");
 
+            let open_or_create_settings_window =
+                |app: &AppHandle| match app.get_webview_window("settings") {
+                    Some(window_settings) => {
+                        window_settings
+                            .unminimize()
+                            .expect("Cannot unminimize window");
+                        window_settings.show().expect("Cannot show window");
+                        window_settings
+                            .set_focus()
+                            .expect("Cannot set focus to window");
+                    }
+                    None => {
+                        let webview_url_settings = WebviewUrl::App("index-settings.html".into());
+                        WebviewWindowBuilder::new(app, "settings", webview_url_settings)
+                            .title("Settings")
+                            .inner_size(1024.0, 768.0)
+                            .build()
+                            .expect("Cannot create settings window");
+                    }
+                };
+
             if is_first_start {
-                let webview_url_settings = WebviewUrl::App("index-settings.html".into());
-                tauri::WebviewWindowBuilder::new(app, "settings", webview_url_settings)
-                    .title("Settings")
-                    .inner_size(1024.0, 768.0)
-                    .build()
-                    .unwrap();
+                open_or_create_settings_window(app.handle());
             }
 
             // Create the tray icon
             // TODO: Add an icon
-            let menu_item_laugh =
-                MenuItem::with_id(app, "laugh", "Laugh", true, None::<&str>).unwrap();
-            let menu = Menu::with_items(app, &[&menu_item_laugh]).expect("Cannot create menu");
-            let mut tray_builder =
-                TrayIconBuilder::new()
-                    .menu(&menu)
-                    .on_menu_event(|_app, event| match event.id.as_ref() {
-                        // TODO: Handle more menu items
-                        "laugh" => {
-                            println!("HAHAHA");
+            let menu_item_settings =
+                MenuItem::with_id(app, "settings", "Open settings", true, None::<&str>).unwrap();
+            let menu_item_quit =
+                MenuItem::with_id(app, "quit", "Quit", true, None::<&str>).unwrap();
+            let menu = Menu::with_items(app, &[&menu_item_settings, &menu_item_quit])
+                .expect("Cannot create menu");
+
+            let open_or_create_settings_window_clone = open_or_create_settings_window.clone();
+            let mut tray_builder = TrayIconBuilder::new()
+                .menu(&menu)
+                .on_menu_event(move |app, event| match event.id.as_ref() {
+                    "settings" => {
+                        open_or_create_settings_window_clone(app);
+                    }
+                    "quit" => {
+                        app.exit(0);
+                    }
+                    _ => {}
+                })
+                .on_tray_icon_event(move |tray, event| match event {
+                    TrayIconEvent::Click {
+                        id: _,
+                        position: _,
+                        rect: _,
+                        button,
+                        button_state,
+                    } => {
+                        if button == MouseButton::Left && button_state == MouseButtonState::Up {
+                            open_or_create_settings_window(tray.app_handle());
                         }
-                        _ => {}
-                    });
+                    }
+                    _ => {}
+                });
 
             #[cfg(not(target_os = "linux"))]
             {
-                tray_builder = tray_builder
-                    .show_menu_on_left_click(false)
-                    .on_tray_icon_event(|_tray, event| match event {
-                        // TODO: Handle more tray events
-                        _ => unimplemented!(),
-                    });
+                tray_builder = tray_builder.show_menu_on_left_click(false)
             }
 
             let _tray = tray_builder.build(app).unwrap();
